629e353945ab36875b1f8c5a7a77cba702770cd1
“KrisReid” <krisreid_06@hotmail.com>
2020-11-24 14:43:21 +0000

    Initial Commit

---
+++ Walking/WalkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkingApp.swift
+//  Walking
+//
+//  Created by Kris Reid on 24/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct WalkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
